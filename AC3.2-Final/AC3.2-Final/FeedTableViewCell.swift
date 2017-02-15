b954c53ddf902b81b708143e1bff7c77bbfb5fcd
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-02-15 11:08:38 -0500

    organized bundle

---
+++ AC3.2-Final/AC3.2-Final/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  
+//
+//  Created by Marty Avedon on 2/15/17.
+//
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
